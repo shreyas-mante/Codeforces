51efac114362b55b61bf67aa7b499289ffb1d069
Shreyas Mante <shreyas.mante@gmail.com>
2021-05-03 00:29:47 +0530

    Codeforces 1.0

---
+++ googleCODEJAM1cA.cpp
@@ -0,0 +1,112 @@
+// im__back - Shreyas Mante
+#include<bits/stdc++.h>
+using namespace std;
+
+/*/-----------------------------TC && FASTIO--------------------------------/*/
+#define nuke ll t; cin>>t; while(t--)
+#define nuke_google ll t; cin>>t; for(ll i_1=1;i_1<=t;i_1++)
+#define case_google cout<<"Case #";
+#define imback ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+/*/-----------------------------MODS&CONSTS---------------------------------/*/
+#define MOD 		1000000007
+#define pi          3.1415926535
+
+/*/-----------------------------FILE--------------------------------/*/
+#define sampleio    freopen("input.txt", "r", stdin);freopen("output.txt", "w", stdout);
+
+/*/-----------------------------DEFINES------------------------------------/*/
+#define ll 			long long int
+#define ld 			long double
+#define pb 			push_back
+#define mk	 		make_pair
+#define fi			first
+#define se			second
+#define ad 			adjacent_difference  //(a,a+6,newarr);// store adjacent diff of array
+#define ita			iota //(a,a+8,100) // store inc values (100,101,102..)
+
+/*/-----------------------------INLINE FUNCTIONS--------------------------/*/
+inline ll gcd(ll a, ll b)          {if (b == 0) return a;    return gcd(b, a % b);}
+inline ll lcm(ll a, ll b)          {return (a / gcd(a, b) * b);}
+inline bool isprime(ll n)            {ll i; for (i = 2; i <= sqrt(n); i++) {if (n % i == 0)return false;} return true;}
+
+
+/*/-----------------------------WRK FUNCTIONS--------------------------/*/
+ll factorial[3000007] = {0};
+void facto() {
+    factorial[0] = 1; factorial[1] = 1; for(int i = 2; i < 3000007; i++)factorial[i] = (factorial[i-1]*i)%MOD;
+}
+
+
+
+/*/-----------------------------NUKE BEGINS-------------------------------/*/
+
+signed main()
+{
+	imback;
+	nuke_google
+	{
+		ll n, k;
+		cin>>n>>k;
+		vector<ll>v(n,0);
+		
+		for(ll i=0;i<n;i++)
+		{
+			cin>>v[i];
+		}
+		
+		sort(v.begin(),v.end());
+		
+		for(ll i=0;i<n;i++)
+		{
+			cout<<v[i]<<" ";
+		}
+		
+		vector<ll>ls;
+		
+		ll s1 , s2;
+		
+		if(v[0] != 1)
+		{
+			s1 = v[0]-1;
+		}
+		
+		for(ll i=1;i<n;i++)
+		{
+			ls.pb(v[i]-v[i-1]-1);
+		}
+		
+		
+		if(v.back()!=k)
+		{
+			s2 = k - v.back();
+		}
+		
+		ll a1 = max(s1,s2);
+		ll ad1 = 0;
+		
+		for(ll i=0;i<ls.size();i++)
+		{
+			ad1 = max(ad1 , (ls[i]+1)/2);
+		}
+		
+		a1 += ad1;
+		ll ans = a1;
+		
+		for(ll i=0;i<ls.size();i++)
+		{
+			ans = max(ans , ls[i]);
+		}
+		
+		long double res = (float)ans/float(k);
+		
+		cout<<fixed;
+		cout<<"Case #"<<i_1<<": "<<setprecision(10)<<res<<endl;
+		
+		
+	}
+}
+
+
+
+
